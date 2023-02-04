b71c29cd3169f5df3396783c65ad52d2719c04b5
Alexey_I <gafar@xaker.ru>
2023-02-04 18:10:39 +0300

    Itogovy script

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+namespace chetnye_kuby
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число N: ");
+            int chisloN = int.Parse(Console.ReadLine());
+            int i = 1;
+            while ( i < chisloN )
+            {
+                double kub = Math.Pow(i, 3);
+                if ( kub % 2 == 0)
+                {
+                Console.WriteLine("Четный куб числа " + i + "  " + kub);
+                }
+                i = i + 1;
+            }
+        }
+    }
+}
